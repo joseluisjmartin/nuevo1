1c5666aad0ad2c02f6e3f41b060c949ed76c436e
Iulius <cyberputamadre>
2017-04-11 19:13:37 +0200

    Primer commit

---
+++ asdada/src/main/java/JL.java
@@ -0,0 +1,12 @@
+
+public class JL {
+    private static int contador;
+
+    public static void main(String[] args){
+        for(int i=0; i<3; i++) {
+            contador++;
+        }
+    }
+
+}
+
